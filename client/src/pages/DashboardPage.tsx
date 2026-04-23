fab7741b875d6e0d59a938fcb5020909957e4a4f
Forrest Miller <forrest.w.miller@icloud.com>
2026-04-22 19:14:58 -0500

    demo - commit

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -3,6 +3,7 @@ const DashboardPage = () => {
     <div className="card" style={{ textAlign: 'center', padding: '40px' }}>
       <h1>Welcome to YogaApp</h1>
       <p>Yoga H'om Studio Management System</p>
+      <p>I'm a convenient paragraph to display my CI/CD pipeline</p>
     </div>
   );
 };
